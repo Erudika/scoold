b850a65c7c692ccdd4de59459f5ecd6a5e5dde39
Alex Bogdanovski <alex@erudika.com>
2017-01-04 20:14:50 +0200

    modules removed

--- scoold-puppet/scoold/files/tokencalc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import sys
-if (len(sys.argv) > 1):
-        num=int(sys.argv[1])
-else:
-        num=int(raw_input("How many nodes are in your cluster? "))
-for i in range(0, num):
-        print 'node %d: %d' % (i, (i*(2**127)/num))
--- scoold-puppet/scoold/lib/puppet/parser/functions/str2int.rb
+++
@@ -1,5 +0,0 @@
-module Puppet::Parser::Functions
-  newfunction(:str2int, :type => :rvalue) do |args|
-    Integer(args[0])
-  end
-end
