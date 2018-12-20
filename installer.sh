c7b45f0ba32a9395b09ba1521ed537b52cdae9bb
Alex Bogdanovski <alex@erudika.com>
2018-12-20 18:50:32 +0200

    added port forwarding for DigitalOcean

--- installer.sh
+++ installer.sh
@@ -3,10 +3,10 @@ set -e -x
 
 # Ubuntu/DigitalOcean installer script
 VERSION="1.31.1"
+PORT="8000"
 sfile="/etc/systemd/system/scoold.service"
-wget https://github.com/Erudika/scoold/releases/download/${VERSION}/scoold-${VERSION}.jar
-chown scoold scoold-*.jar
-chmod +x scoold-*.jar
+wget https://github.com/Erudika/scoold/releases/download/${VERSION}/scoold-${VERSION}.jar && \
+chown scoold scoold-*.jar && chmod +x scoold-*.jar
 
 touch $sfile
 cat << EOF > $sfile
@@ -16,11 +16,14 @@ After=syslog.target
 [Service]
 WorkingDirectory=/home/scoold
 SyslogIdentifier=Scoold
-ExecStart=/bin/bash -c "java -jar -Dserver.port=8000 -Dconfig.file=/home/scoold/application.conf scoold-*.jar"
+ExecStart=/bin/bash -c "java -jar -Dserver.port=${PORT} -Dconfig.file=/home/scoold/application.conf scoold-*.jar"
 User=scoold
 [Install]
 WantedBy=multi-user.target
 EOF
+
+iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-port ${PORT} && \
+iptables -t nat -A OUTPUT -p tcp --dport 80 -o lo -j REDIRECT --to-port ${PORT}
 
 systemctl enable scoold.service && \
 systemctl start scoold.service
