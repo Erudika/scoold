6c39066e49ebc6144aaa8ef6ed45c0da7c09418a
Alex Bogdanovski <alex@erudika.com>
2019-06-26 21:22:03 +0300

    added .travis.yml

--- src/main/resources/static/scripts/scoold.js
+++ src/main/resources/static/scripts/scoold.js
@@ -1,7 +1,19 @@
 /*
- * Unobtrusive javascript for scoold.com
- * author: Alexander Bogdanovski
- * (CC) BY-SA
+ * Copyright 2013-2019 Erudika. https://erudika.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For issues and patches go to: https://github.com/erudika
  */
 /*global window: false, jQuery: false, $: false, CSRF_COOKIE, google, hljs, RTL_ENABLED, CONTEXT_PATH, M: false */
 "use strict";
