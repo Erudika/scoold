c78c28320b67091a3c2bd36753834d85561d9107
Alex Bogdanovski <alex@erudika.com>
2020-01-25 01:37:52 +0200

    added tag rename feature, closes #144

--- src/main/java/com/erudika/scoold/controllers/TagsController.java
+++ src/main/java/com/erudika/scoold/controllers/TagsController.java
@@ -17,21 +17,28 @@
  */
 package com.erudika.scoold.controllers;
 
+import com.erudika.para.client.ParaClient;
 import com.erudika.para.core.Tag;
 import com.erudika.para.utils.Pager;
+import com.erudika.para.utils.Utils;
 import static com.erudika.scoold.ScooldServer.SIGNINLINK;
 import static com.erudika.scoold.ScooldServer.TAGSLINK;
 import com.erudika.scoold.core.Profile;
+import com.erudika.scoold.core.Question;
 import com.erudika.scoold.utils.ScooldUtils;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,11 +51,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/tags")
 public class TagsController {
 
+	private static final Logger logger = LoggerFactory.getLogger(TagsController.class);
+
 	private final ScooldUtils utils;
+	private final ParaClient pc;
 
 	@Inject
 	public TagsController(ScooldUtils utils) {
 		this.utils = utils;
+		this.pc = utils.getParaClient();
 	}
 
 	@GetMapping
@@ -62,7 +73,7 @@ public class TagsController {
 		String currentSpace = utils.getSpaceIdFromCookie(authUser, req);
 		List<Tag> tagslist = Collections.emptyList();
 		if (utils.canAccessSpace(authUser, currentSpace)) {
-			tagslist = utils.getParaClient().findTags("", itemcount);
+			tagslist = pc.findTags("", itemcount);
 		} else if (!utils.isDefaultSpacePublic()) {
 			return "redirect:" + SIGNINLINK + "?returnto=" + TAGSLINK;
 		}
@@ -75,9 +86,46 @@ public class TagsController {
 		return "base";
 	}
 
+	@PostMapping
+	public String rename(@RequestParam String tag, @RequestParam String newtag, HttpServletRequest req, Model model) {
+		Profile authUser = utils.getAuthUser(req);
+		if (utils.isMod(authUser)) {
+			Tag tagg = new Tag(tag);
+			Tag tagg2 = new Tag(newtag);
+			Tag t = pc.read(Utils.type(Tag.class), tagg.getId());
+			if (t != null && !tagg.getTag().equals(tagg2.getTag())) {
+				pc.delete(t);
+				t.setId(newtag);
+				logger.info("User {} ({}) is renaming tag '{}' to '{}'.",
+						authUser.getName(), authUser.getId(), tagg.getTag(), t.getTag());
+
+				int taggedCount = 0;
+				Pager pager = new Pager(1, "_docid", false, 25);
+				List<Question> questionslist;
+				do {
+					questionslist = pc.findTagged(Utils.type(Question.class), new String[]{tagg.getTag()}, pager);
+					for (Question q : questionslist) {
+						taggedCount++;
+						q.setTags(q.getTags().stream().
+								map(ts -> ts.equals(tagg.getTag()) ? t.getTag() : ts).
+								collect(Collectors.toList()));
+						logger.debug("Updated {} out of {} questions with new tag {}.",
+								questionslist.size(), pager.getCount(), t.getTag());
+					}
+					if (!questionslist.isEmpty()) {
+						pc.updateAll(questionslist);
+					}
+				} while (!questionslist.isEmpty());
+				t.setCount(taggedCount);
+				pc.create(t);
+			}
+		}
+		return "redirect:" + TAGSLINK;
+	}
+
 	@ResponseBody
 	@GetMapping(path = "/{keyword}", produces = MediaType.APPLICATION_JSON)
 	public List<?> findTags(@PathVariable String keyword) {
-		return utils.getParaClient().findTags(keyword, new Pager(10));
+		return pc.findTags(keyword, new Pager(10));
 	}
 }
