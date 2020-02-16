d16d684f1c66899935011011796f74f1aecf6953
Alex Bogdanovski <alex@erudika.com>
2020-02-17 01:20:54 +0200

    Release v1.37.0.

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 # Lightsail/DigitalOcean installer script for Ubuntu
-VERSION="1.36.2"
+VERSION="1.37.0"
 PORT="8000"
 WORKDIR="/home/ubuntu"
 JARURL="https://github.com/Erudika/scoold/releases/download/${VERSION}/scoold-${VERSION}.jar"
