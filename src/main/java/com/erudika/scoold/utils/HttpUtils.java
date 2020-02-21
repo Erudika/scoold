9d73fac640cb5a5a568faf1d1fb1c040e5e2eef9
Alex Bogdanovski <alex@erudika.com>
2020-02-21 16:16:07 +0200

    bumped year in license

--- src/main/java/com/erudika/scoold/utils/HttpUtils.java
+++ src/main/java/com/erudika/scoold/utils/HttpUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2019 Erudika. http://erudika.com
+ * Copyright 2013-2020 Erudika. http://erudika.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
