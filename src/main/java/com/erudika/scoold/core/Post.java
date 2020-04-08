d329c3ab31817fdfe1840d181cdd632359d07fa1
Romeu Rosa <romeu.rosa@talkdesk.com>
2020-04-08 10:53:52 +0100

    Change default space to integrations
    Add it as a default assignment to new users

--- src/main/java/com/erudika/scoold/core/Post.java
+++ src/main/java/com/erudika/scoold/core/Post.java
@@ -51,7 +51,7 @@ public abstract class Post extends Sysprop {
 
 	private static final long serialVersionUID = 1L;
 
-	public static final String DEFAULT_SPACE = "scooldspace:default";
+	public static final String DEFAULT_SPACE = "scooldspace:integrations";
 	public static final String ALL_MY_SPACES = "scooldspace:*";
 
 	@Stored
