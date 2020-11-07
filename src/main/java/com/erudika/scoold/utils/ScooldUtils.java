94b5a846045e6604c03e3350356f335088b6d843
Alex Bogdanovski <alex@erudika.com>
2020-11-07 11:50:02 +0200

    fixed possible UnsupportedOperationException

--- src/main/java/com/erudika/scoold/utils/ScooldUtils.java
+++ src/main/java/com/erudika/scoold/utils/ScooldUtils.java
@@ -926,7 +926,7 @@ public final class ScooldUtils {
 
 	public List<Sysprop> getAllSpaces() {
 		if (allSpaces == null || allSpaces.isEmpty()) {
-			allSpaces = pc.findQuery("scooldspace", "*", new Pager(Config.DEFAULT_LIMIT));
+			allSpaces = new LinkedList<>(pc.findQuery("scooldspace", "*", new Pager(Config.DEFAULT_LIMIT)));
 		}
 		return allSpaces;
 	}
