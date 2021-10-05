193be14347fc6f8884b1814dcf0103e31ef2e1de
Alex Bogdanovski <alex@erudika.com>
2021-10-05 16:07:55 +0300

    fixed unescaped <> chars cause invalid search query syntax errors, closes #267

--- src/main/java/com/erudika/scoold/utils/ScooldUtils.java
+++ src/main/java/com/erudika/scoold/utils/ScooldUtils.java
@@ -1220,7 +1220,7 @@ public final class ScooldUtils {
 		String defaultQuery = "*";
 		String q = StringUtils.trimToEmpty(query);
 		if (qf.isEmpty() || qf.length() > 1) {
-			q = q.replaceAll("[\\?]", "").trim();
+			q = q.replaceAll("[\\?<>]", "").trim();
 			q = q.replaceAll("$[\\*]*", "");
 			q = RegExUtils.removeAll(q, "AND");
 			q = RegExUtils.removeAll(q, "OR");
