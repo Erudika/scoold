6f64c9f502890dbf7bd37291525f3a951044da71
Alex Bogdanovski <alex@erudika.com>
2021-10-10 13:29:55 +0300

    fixed language file structure for EN, FR

--- src/main/java/com/erudika/scoold/controllers/SearchController.java
+++ src/main/java/com/erudika/scoold/controllers/SearchController.java
@@ -147,9 +147,9 @@ public class SearchController {
 				+ "<OpenSearchDescription xmlns=\"http://a9.com/-/spec/opensearch/1.1/\" "
 				+ "  xmlns:moz=\"http://www.mozilla.org/2006/browser/search/\">\n"
 				+ "  <ShortName>" + Config.APP_NAME + "</ShortName>\n"
-				+ "  <Description>" + utils.getLang(req).get("opensearch.description") + "</Description>\n"
+				+ "  <Description>" + utils.getLang(req).get("search.description") + "</Description>\n"
 				+ "  <InputEncoding>UTF-8</InputEncoding>\n"
-				+ "  <Image width=\"16\" height=\"16\" type=\"image/x-icon\">" + 
+				+ "  <Image width=\"16\" height=\"16\" type=\"image/x-icon\">" +
 				ScooldServer.getServerURL() + CONTEXT_PATH + "/favicon.ico</Image>\n"
 				+ "  <Url type=\"text/html\" method=\"get\" template=\"" + ScooldServer.getServerURL() + CONTEXT_PATH
 				+ "/search?q={searchTerms}\"></Url>\n"
