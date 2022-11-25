704dca23c7dece4f1cb96c43798092e6e15ae10e
Alex Bogdanovski <alex@erudika.com>
2022-11-25 14:04:39 +0200

    added 405 error exception handler to take care of log spam

---
+++ src/main/java/com/erudika/scoold/utils/RequestNotSupportedExceptionHandler.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013-2022 Erudika. https://erudika.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For issues and patches go to: https://github.com/erudika
+ */
+package com.erudika.scoold.utils;
+
+import java.util.Set;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * HttpRequestMethodNotSupportedException handler - suppress spammy log messages.
+ * @author Alex Bogdanovski [alex@erudika.com]
+ */
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RequestNotSupportedExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		Set<HttpMethod> supportedMethods = ex.getSupportedHttpMethods();
+		if (!CollectionUtils.isEmpty(supportedMethods)) {
+			headers.setAllow(supportedMethods);
+		}
+		return new ResponseEntity<>(null, headers, status);
+	}
+
+}
