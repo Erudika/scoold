160132f152672d7136b9e5423ea3fe2a712b1db7
Alex Bogdanovski <alex@erudika.com>
2025-12-21 20:13:21 +0200

    updated year in header

--- src/main/java/com/erudika/scoold/controllers/CommentController.java
+++ src/main/java/com/erudika/scoold/controllers/CommentController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2022 Erudika. https://erudika.com
+ * Copyright 2013-2026 Erudika. https://erudika.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
