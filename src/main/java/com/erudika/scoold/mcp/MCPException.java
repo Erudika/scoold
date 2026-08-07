e0679e503619e5e91f1c9b08217bc9fa70d917b5
Alex Bogdanovski <alex@erudika.com>
2026-08-07 16:45:11 +0300

    fixed license header

--- src/main/java/com/erudika/scoold/mcp/MCPException.java
+++ src/main/java/com/erudika/scoold/mcp/MCPException.java
@@ -1,7 +1,11 @@
 /*
  * Copyright 2013-2026 Erudika. https://erudika.com
  *
- * Licensed under the EULA - use is subject to license terms.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -9,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * For issues and patches go to: https://github.com/erudika/scoold-pro
+ * For issues and patches go to: https://github.com/erudika
  */
 package com.erudika.scoold.mcp;
 
